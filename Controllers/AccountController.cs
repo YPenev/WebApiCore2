be607fb2dd59a018cee7f78e780b26af42b81d9a
Penev <dany.penev@gmail.com>
2018-02-18 15:25:09 +0200

    Added web client

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -39,7 +39,7 @@ namespace WebApiJwt.Controllers
         [HttpPost]
         [EnableCors("AllowAll")]
         public async Task<object> Login([FromBody] LoginDto model)
-        {
+        { 
             var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
             
             if (result.Succeeded)
